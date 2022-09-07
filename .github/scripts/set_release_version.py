7f4a86c48a94fd2cf1b85e94a7262cf971e10fe8
Aaron Webster <awebster@gmail.com>
2022-09-07 11:47:34 -0700

    Create automatic releases via github workflow.

---
+++ .github/scripts/set_release_version.py
@@ -0,0 +1,7 @@
+# Obtains a date-based version for the release workflow.
+
+import datetime
+
+now_utc = datetime.datetime.now(datetime.timezone.utc)
+version = now_utc.strftime('%Y.%m%d.%H%M%S')
+print('::set-output name=version::v{}'.format(version))
