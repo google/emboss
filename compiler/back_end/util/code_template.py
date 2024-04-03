b77e83941a936e7439d90899c74714894aae5b7d
Eric Rahm <erahm@google.com>
2024-04-03 15:10:32 -0700

    Switch code_templates to string.Template
    Python's standard `string.Template` is significantly more performant
    than the custom `code_template` formatter. At the cost of removing
    custom formatting support (unused) and changing our format string to
    `${foo}` instead of `$_foo_$` we can achieve an 11% speedup in the back
    end code generation for C++.

--- compiler/back_end/util/code_template.py
+++ compiler/back_end/util/code_template.py
@@ -22,82 +22,25 @@ import re
 import string
 
 
-class _CppFormatter(string.Formatter):
-  """Customized Formatter using $_name_$ instead of {name}.
-
-  This class exists for the format_template() function; see its documentation
-  for details.
-  """
-
-  def parse(self, format_string):
-    """Overrides string.Formatter.parse.
-
-    Arguments:
-      format_string: a format string to be parsed.
-
-    Yields:
-      A sequence of 4-element tuples (literal, name, format_spec, conversion),
-      where:
-
-        literal: A literal string to include in the output.  This will be
-          output before the substitution, if any.
-        name: The name of a substitution, or None if no substitution.
-        format_spec: A format specification.
-        conversion: A conversion specification.
-
-      Consult the documentation for string.Formatter for the format of the
-      format_spec and conversion elements.
-    """
-    # A replacement spec is $_field_name!conversion:format_spec_$, where
-    # conversion and format_spec are optional.  string.Formatter will take care
-    # of parsing and interpreting the conversion and format_spec, so this method
-    # just extracts them.
-    delimiter_matches = re.finditer(
-        r"""(?x)
-            \$_
-                  (?P<field_name>  ( [^!:_] | _[^$] )* )
-              ( ! (?P<conversion>  ( [^:_]  | _[^$] )* ) )?
-              ( : (?P<format_spec> ( [^_]   | _[^$] )* ) )?
-            _\$""", format_string)
-    after_last_delimiter = 0
-    for match in delimiter_matches:
-      yield (format_string[after_last_delimiter:match.start()],
-             match.group("field_name"),
-             # A missing format_spec is indicated by ""...
-             match.group("format_spec") or "",
-             # ... but a missing conversion is indicated by None.  Consistency!
-             match.group("conversion") or None)
-      after_last_delimiter = match.end()
-    yield format_string[after_last_delimiter:], None, None, None
-
-
-_FORMATTER = _CppFormatter()
-
-
-def format_template(template, *args, **kwargs):
-  """format_template acts like str.format, but uses $_name_$ instead of {name}.
+def format_template(template, **kwargs):
+  """format_template acts like str.format, but uses ${name} instead of {name}.
 
   format_template acts like a str.format, except that instead of using { and }
-  to delimit substitutions, format_template uses $_ and _$.  This simplifies
+  to delimit substitutions, format_template uses ${name}.  This simplifies
   templates of source code in most languages, which frequently use "{" and "}",
-  but very rarely use "$".  The choice of "$_" and "_$" is conducive to the use
-  of clang-format on templates.
+  but very rarely use "$".
 
-  format_template does not currently have a way to put literal "$_..._$" into a
-  format string.
-
-  See the documentation for str.format and string.Formatter for details about
+  See the documentation for string.Template for details about
   template strings and the format of substitutions.
 
   Arguments:
     template: A template to format.
-    *args: Positional arguments for string.Formatter.format.
-    **kwargs: Keyword arguments for string.Formatter.format.
+    **kwargs: Keyword arguments for string.Template.substitute.
 
   Returns:
     A formatted string.
   """
-  return _FORMATTER.format(template, *args, **kwargs)
+  return template.substitute(**kwargs)
 
 
 def parse_templates(text):
@@ -126,15 +69,18 @@ def parse_templates(text):
   templates = {}
   name = None
   template = []
+  def finish_template(template):
+    return string.Template("\n".join(template))
+
   for line in text.splitlines():
     if delimiter_re.match(line):
       if name:
-        templates[name] = "\n".join(template)
+        templates[name] = finish_template(template)
       name = delimiter_re.match(line).group(1)
       template = []
     else:
       template.append(line)
   if name:
-    templates[name] = "\n".join(template)
+    templates[name] = finish_template(template)
   return collections.namedtuple("Templates",
                                 list(templates.keys()))(**templates)
