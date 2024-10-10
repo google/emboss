78901f985eb3a387ffb636a1bd35821cde995ae9
Dmitri Prime <bolms@google.com>
2024-10-10 11:35:34 -0700

    Cache the generated parser tables as a .py file (#196)
    This change adds a checked in, cached copy of the parser tables,
    which will normally be used instead of generating a fresh set of
    tables.  On my machine, this saves approximately 2.05s on first
    run of `embossc`, and 4.96s after `.pyc` files have been generated.
    
    For a small `.emb` file, those savings translate to approximately 36%
    and 87% of the current total runtime, respectively.
    
    In order to minimize frustrating cache decoherence problems, there are
    two additional features:
    
    *   When loading the cached parser, the set of grammar productions that
        were used to generate the cached parser is compared to the current
        set.  If they do not match, the cached parser is ignored, and a
        fresh parser is generated.  This allows developers to work on
        `module_ir.py` without manually regenerating the parser tables on
        every change.
    *   The new `cached_parser_is_up_to_date_test` will fail if the cached
        parser text is not an *exact* match for the text that would be
        generated from a fresh run of `generate_cached_parser`.  This
        ensures that the version of `cached_parser.py` that is in the
        master Emboss repo is always up to date, no matter what might have
        changed in the source tree.

--- compiler/front_end/parser.py
+++ compiler/front_end/parser.py
@@ -12,113 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Routines to generate a shift-reduce parser from the module_ir module."""
+"""Routines to load a shift-reduce parser for the module_ir module."""
 
+from compiler.front_end.generated import cached_parser
 from compiler.front_end import lr1
+from compiler.front_end import make_parser
 from compiler.front_end import module_ir
-from compiler.front_end import tokenizer
-from compiler.util import resources
+from compiler.util import parser_types
 from compiler.util import simple_memoizer
-
-
-class ParserGenerationError(Exception):
-    """An error occurred during parser generation."""
-
-    pass
-
-
-def parse_error_examples(error_example_text):
-    """Parses error examples from error_example_text.
-
-    Arguments:
-      error_example_text: The text of an error example file.
-
-    Returns:
-      A list of tuples, suitable for passing into generate_parser.
-
-    Raises:
-      ParserGenerationError: There is a problem parsing the error examples.
-    """
-    error_examples = error_example_text.split("\n" + "=" * 80 + "\n")
-    result = []
-    # Everything before the first "======" line is explanatory text: ignore it.
-    for error_example in error_examples[1:]:
-        message_and_examples = error_example.split("\n" + "-" * 80 + "\n")
-        if len(message_and_examples) != 2:
-            raise ParserGenerationError(
-                "Expected one error message and one example section in:\n"
-                + error_example
-            )
-        message, example_text = message_and_examples
-        examples = example_text.split("\n---\n")
-        for example in examples:
-            # TODO(bolms): feed a line number into tokenize, so that tokenization
-            # failures refer to the correct line within error_example_text.
-            tokens, errors = tokenizer.tokenize(example, "")
-            if errors:
-                raise ParserGenerationError(str(errors))
-
-            for i in range(len(tokens)):
-                if tokens[i].symbol == "BadWord" and tokens[i].text == "$ANY":
-                    tokens[i] = lr1.ANY_TOKEN
-
-            error_token = None
-            for i in range(len(tokens)):
-                if tokens[i].symbol == "BadWord" and tokens[i].text == "$ERR":
-                    error_token = tokens[i + 1]
-                    del tokens[i]
-                    break
-            else:
-                raise ParserGenerationError(
-                    "No error token marker '$ERR' in:\n" + error_example
-                )
-
-            result.append((tokens, error_token, message.strip(), example))
-    return result
-
-
-def generate_parser(start_symbol, productions, error_examples):
-    """Generates a parser from grammar, and applies error_examples.
-
-    Arguments:
-        start_symbol: the start symbol of the grammar (a string)
-        productions: a list of parser_types.Production in the grammar
-        error_examples: A list of (source tokens, error message, source text)
-            tuples.
-
-    Returns:
-        A parser.
-
-    Raises:
-        ParserGenerationError: There is a problem generating the parser.
-    """
-    parser = lr1.Grammar(start_symbol, productions).parser()
-    if parser.conflicts:
-        raise ParserGenerationError("\n".join([str(c) for c in parser.conflicts]))
-    for example in error_examples:
-        mark_result = parser.mark_error(example[0], example[1], example[2])
-        if mark_result:
-            raise ParserGenerationError(
-                "error marking example: {}\nExample:\n{}".format(
-                    mark_result, example[3]
-                )
-            )
-    return parser
 
 
 @simple_memoizer.memoize
 def _load_module_parser():
-    error_examples = parse_error_examples(
-        resources.load("compiler.front_end", "error_examples")
-    )
-    return generate_parser(
-        module_ir.START_SYMBOL, module_ir.PRODUCTIONS, error_examples
-    )
+    module_parser = cached_parser.module_parser()
+    if module_parser.productions == set(module_ir.PRODUCTIONS) | {
+        parser_types.Production(lr1.START_PRIME, (module_ir.START_SYMBOL,))
+    }:
+        return module_parser
+    return make_parser.build_module_parser()
 
 
 @simple_memoizer.memoize
 def _load_expression_parser():
-    return generate_parser(module_ir.EXPRESSION_START_SYMBOL, module_ir.PRODUCTIONS, [])
+    expression_parser = cached_parser.expression_parser()
+    if expression_parser.productions == set(module_ir.PRODUCTIONS) | {
+        parser_types.Production(lr1.START_PRIME, (module_ir.EXPRESSION_START_SYMBOL,))
+    }:
+        return expression_parser
+    return make_parser.build_expression_parser()
 
 
 def parse_module(tokens):
